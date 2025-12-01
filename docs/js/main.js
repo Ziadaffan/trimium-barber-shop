cff6615c02db862e9607a1dea928d6b7609bb003
zaffan-nurova <ziad.affan@nurova.ca>
2025-12-01 10:56:38 -0500

    Revamp barber shop website design with updated HTML structure, enhanced CSS styles, and improved JavaScript functionality. Introduced new fonts, background gradients, and responsive elements for a modern look. Enhanced user experience with a preloader, sticky header, and refined appointment form validation. Updated product and gallery sections for better presentation and interaction.

--- docs/js/main.js
+++ docs/js/main.js
@@ -217,99 +217,9 @@
     return `${displayHours.toString().padStart(2, "0")}:${minutes} ${period}`;
   }
 
-  /*=========================================================================
-	Sticky Header
-=========================================================================*/
-  $("#header").after('<div class="header-height"></div>');
-  function headerHeight() {
-    var height = $("#header").height();
-    $(".header-height").css("height", height + "px");
-  }
-  $(function () {
-    var header = $("#header"),
-      yOffset = 0,
-      triggerPoint = 80;
-    headerHeight();
-    $(window).resize(headerHeight);
-    $(window).on("scroll", function () {
-      yOffset = $(window).scrollTop();
 
-      if (yOffset >= triggerPoint) {
-        header.addClass("navbar-fixed-top animated slideInDown");
-      } else {
-        header.removeClass("navbar-fixed-top animated slideInDown");
-      }
-    });
-  });
 
-  /*=========================================================================
-    Mobile Menu
-=========================================================================*/
-  $(".menu-wrap ul.nav").slicknav({
-    prependTo: ".header-section .navbar",
-    label: "",
-    allowParentLinks: true,
-  });
 
-  /*=========================================================================
-        Vdeo Background
-=========================================================================*/
-  $(".video_bg").YTPlayer();
-
-  /*=========================================================================
-    Main Slider
-=========================================================================*/
-  $("#main-slider").owlCarousel({
-    loop: true,
-    autoplay: true,
-    smartSpeed: 500,
-    items: 1,
-    nav: true,
-    navText: [
-      '<i class="arrow_carrot-left"></i>',
-      '<i class="arrow_carrot-right"></i>',
-    ],
-  });
-
-  $("#main-slider").on("translate.owl.carousel", function () {
-    $(".main_slide .slider_content h3")
-      .removeClass("animated fadeInUp")
-      .css("opacity", "0");
-    $(".main_slide .slider_content h1")
-      .removeClass("animated fadeInUp")
-      .css("opacity", "0");
-    $(".main_slide .slider_content p, .main_slide .slider_content .default_btn")
-      .removeClass("animated fadeInUp")
-      .css("opacity", "0");
-  });
-
-  $("#main-slider").on("translated.owl.carousel", function () {
-    $(".main_slide .slider_content h3")
-      .addClass("animated fadeInUp")
-      .css("opacity", "1");
-    $(".main_slide .slider_content h1")
-      .addClass("animated fadeInUp")
-      .css("opacity", "1");
-    $(".main_slide .slider_content p, .main_slide .slider_content .default_btn")
-      .addClass("animated fadeInUp")
-      .css("opacity", "1");
-  });
-
-  /*=========================================================================
-    Gallery Slider
-=========================================================================*/
-  $("#gallery-slide").owlCarousel({
-    loop: true,
-    autoplay: true,
-    smartSpeed: 500,
-    items: 1,
-    dots: false,
-    nav: true,
-    navText: [
-      '<i class="arrow_carrot-left"></i>',
-      '<i class="arrow_carrot-right"></i>',
-    ],
-  });
 
   /*=========================================================================
     Isotope Active
@@ -344,16 +254,6 @@
     offset: 60,
   });
 
-  /*=========================================================================
-    Testimonial Carousel
-=========================================================================*/
-  $("#testimonial_carousel").owlCarousel({
-    loop: true,
-    autoplay: true,
-    smartSpeed: 500,
-    items: 1,
-    nav: false,
-  });
 
   /*=========================================================================
     Products Carousel
@@ -362,7 +262,7 @@
     loop: true,
     autoplay: true,
     smartSpeed: 500,
-    margin: 30,
+    margin: 15,
     nav: true,
     navText: [
       '<i class="arrow_carrot-left"></i>',
@@ -372,15 +272,23 @@
     responsive: {
       0: {
         items: 1,
+        margin: 10,
       },
-      480: {
-        items: 2,
+      640: {
+        items: 1,
+        margin: 15,
       },
       768: {
-        items: 3,
+        items: 2,
+        margin: 20,
       },
       1024: {
+        items: 3,
+        margin: 25,
+      },
+      1280: {
         items: 4,
+        margin: 30,
       },
     },
   });
@@ -390,57 +298,6 @@
 =========================================================================*/
   $("select:not(#app_barbers, #app_services)").niceSelect();
 
-  /*=========================================================================
-    Food Carousel
-=========================================================================*/
-  $("#food_carousel").imagesLoaded(function () {
-    $("#food_carousel").owlCarousel({
-      loop: true,
-      margin: 10,
-      autoplay: true,
-      smartSpeed: 500,
-      nav: false,
-      dots: false,
-      responsive: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        480: {
-          items: 3,
-        },
-        768: {
-          items: 4,
-        },
-      },
-    });
-  });
-
-  /*=========================================================================
-    Sponsor Carousel
-=========================================================================*/
-  $("#sponsor_carousel").imagesLoaded(function () {
-    $("#sponsor_carousel").owlCarousel({
-      loop: true,
-      margin: 10,
-      autoplay: true,
-      smartSpeed: 500,
-      nav: false,
-      dots: false,
-      responsive: true,
-      responsive: {
-        0: {
-          items: 2,
-        },
-        480: {
-          items: 3,
-        },
-        768: {
-          items: 6,
-        },
-      },
-    });
-  });
 
   /*=========================================================================
     Active venobox
@@ -454,47 +311,4 @@
 	WOW Active
 =========================================================================*/
   new WOW().init();
-  /*=========================================================================
-    Scroll To Top
-=========================================================================*/
-  $(window).on("scroll", function () {
-    if ($(this).scrollTop() > 100) {
-      $("#scroll-to-top").fadeIn();
-    } else {
-      $("#scroll-to-top").fadeOut();
-    }
-  });
-
-  /*=========================================================================
-    MAILCHIMP
-=========================================================================*/
-
-  if ($(".subscribe_form").length > 0) {
-    /*  MAILCHIMP  */
-    $(".subscribe_form").ajaxChimp({
-      language: "es",
-      callback: mailchimpCallback,
-      url: "//alexatheme.us14.list-manage.com/subscribe/post?u=48e55a88ece7641124b31a029&amp;id=361ec5b369",
-    });
-  }
-
-  function mailchimpCallback(resp) {
-    if (resp.result === "success") {
-      $("#subscribe-result").addClass("subs-result");
-      $(".subscription-success").text(resp.msg).fadeIn();
-      $(".subscription-error").fadeOut();
-    } else if (resp.result === "error") {
-      $("#subscribe-result").addClass("subs-result");
-      $(".subscription-error").text(resp.msg).fadeIn();
-    }
-  }
-  $.ajaxChimp.translations.es = {
-    submit: "Submitting...",
-    0: "We have sent you a confirmation email",
-    1: "Please enter your email",
-    2: "An email address must contain a single @",
-    3: "The domain portion of the email address is invalid (the portion after the @: )",
-    4: "The username portion of the email address is invalid (the portion before the @: )",
-    5: "This email address looks fake or invalid. Please enter a real email address",
-  };
 })(jQuery);
